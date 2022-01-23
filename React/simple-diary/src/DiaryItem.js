55d2181688e2a9ea9e21da9f6f61cb940e5475d2
Sangji <snagllee20@gmail.com>
2022-01-23 18:58:18 +0900

    DiaryItem isEdit_State생성 toggleisEdit_함수생성

--- React/simple-diary/src/DiaryItem.js
+++ React/simple-diary/src/DiaryItem.js
@@ -1,4 +1,5 @@
-// 1. props 받아와주기
+import { useState } from "react";
+
 const DiaryItem = ({
   onRemove,
   author,
@@ -7,6 +8,13 @@ const DiaryItem = ({
   emotion,
   id,
 }) => {
+  // < 6-6 데이터 수정하기 >
+  // isEdit State 생성 | toggleisEdit 함수생성(-> isEdit 상태 값을 반전시킴)
+  const [isEdit, setisEdit] = useState(false);
+  const toggleisEdit = () => {
+    setisEdit(!isEdit);
+  };
+
   const handleRemove = () => {
     if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?!`)) {
       onRemove(id); // 함수 호출 (----> App컴포넌트 onRemove)
@@ -23,9 +31,9 @@ const DiaryItem = ({
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
-
       <button onClick={handleRemove}>삭제하기</button>
-      <button>수정하기</button>
+      <button onClick={toggleisEdit}>수정하기</button>{" "}
+      {/* onClick에 toggleisEdit 함수 넣어주기 */}
     </div>
   );
 };
