b37809434192a1c2f3ba3cc80d7fe3392c3eeb42
Sangji <snagllee20@gmail.com>
2022-01-27 19:06:21 +0900

    App컴포넌트에 data state생성, onCreate함수 생성

--- React/simple-diary-review/src/App.js
+++ React/simple-diary-review/src/App.js
@@ -1,42 +1,35 @@
 import "./App.css";
-// import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
 
-const dummyList = [
-  {
-    id: 1,
-    author: "Sang",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    emotion: 3,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 2,
-    author: "Mina",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    emotion: 5,
-    created_date: new Date().getTime(),
-  },
-  {
-    id: 3,
-    author: "Alicia",
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    emotion: 1,
-    created_date: new Date().getTime(),
-  },
-];
+function App() {
+  const [data, setdata] = useState([]);
 
-const App = () => {
+  // id값 변수 - 0번 부터 시작
+  const dataId = useRef(0);
+
+  // 일기 배열에 새로운 일기 추가 함수
+  const onCreate = (author, content, emotion) => {
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    };
+    dataId.current += 1; // id값 한번 쓸 때 마다 +1
+    setdata([newItem, ...data]);
+    // 새로운 일기데이터를 맨 위로, 그 밑으로 기존 데이터들
+  };
+
   return (
     <div>
       <DiaryEditor />
-      <DiaryList diaryList={dummyList} />
+      <DiaryList diaryList={[]} />
     </div>
   );
-};
+}
 
 export default App;
