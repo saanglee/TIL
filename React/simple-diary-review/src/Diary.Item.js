8cacf10e684398d3f451920f558235d9924e71b6
Sangji <snagllee20@gmail.com>
2022-01-27 17:45:21 +0900

    CSS

--- React/simple-diary-review/src/Diary.Item.js
+++ React/simple-diary-review/src/Diary.Item.js
@@ -6,7 +6,7 @@ const DiaryItem = ({ author, content, created_date, emotion, id }) => {
           작성자 : {author} | 감정점수 : {emotion}
         </span>
         <br />
-        <span className="date">{new Date(created_date).toLocaleString}</span>
+        <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
       <div className="content">{content}</div>
     </div>
