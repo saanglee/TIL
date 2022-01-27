9073452e1e73833daec549fa1e5ec3e5b7474493
Sangji <snagllee20@gmail.com>
2022-01-27 17:14:14 +0900

    map example2

--- JavaScript/example.js
+++ JavaScript/example.js
@@ -1,16 +1,28 @@
 // Using map to reformat objects in an array
 
-let kvArr = [
-  { key: 1, value: 10 },
-  { key: 2, value: 20 },
-  { key: 3, value: 30 },
+let arr = [
+  { id: 1, pw: 123 },
+  { id: 2, pw: 456 },
+  { id: 3, pw: 789 },
 ];
 
-let reformattedArray = kvArr.map((obj) => {
+let reformattedArray = arr.map((obj) => {
   let rObj = {};
-  rObj[obj.key] = obj.value;
+  rObj[obj.id] = obj.pw;
   return rObj;
 });
 
 console.log(reformattedArray);
-// [ { '1': 10 }, { '2': 20 }, { '3': 30 } ]
+// reformattedArray is now [ { '1': 123 }, { '2': 456 }, { '3': 789 } ]
+
+let reformattedArray2 = arr.map((obj) => {
+  let rObj2 = {};
+  (rObj2["아이디"] = obj.id), (rObj2["비밀번호"] = obj.pw);
+  return rObj2;
+});
+
+console.log(reformattedArray2);
+// reformattedArray2 is now
+// [ { '아이디': 1, '비밀번호': 123 },
+//   { '아이디': 2, '비밀번호': 456 },
+//   { '아이디': 3, '비밀번호': 789 } ]
