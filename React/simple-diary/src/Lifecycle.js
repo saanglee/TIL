1df5f0ad1ff5cdff5719896e59d957e684f8b742
Sangji <snagllee20@gmail.com>
2022-01-28 15:28:30 +0900

    Lifecycle - Unmount 다뤄보기

--- React/simple-diary/src/Lifecycle.js
+++ React/simple-diary/src/Lifecycle.js
@@ -1,42 +1,29 @@
 import React, { useEffect, useState } from "react";
 
-const Lifecycle = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState("");
-
-  // 컴포넌트 마운트
+// 하나의 파일에 두개의 컴포넌트도 가능함
+const UnmountTest = () => {
+  // 컴포넌트가 Unmount되는 순간 제어하기
   useEffect(() => {
+    // Mount
     console.log("Mount!");
+    // Unmount
+    return () => {
+      console.log("Unmount!");
+    };
   }, []);
 
-  // 컴포넌트 업데이트
-  useEffect(() => {
-    console.log("Update!");
-  });
+  return <div>Unmount Testing Component</div>;
+};
 
-  // count state가 변하는 순간 콜백함수 실행
-  useEffect(() => {
-    console.log(`count is update ${count}`);
-    if (count > 5) {
-      alert("count가 5를 넘었습니다. 따라서 1로 초기화 합니다.");
-      setCount(1);
-    }
-  }, [count]);
-
-  // text state가 변하는 순간 콜백함수 실행
-  useEffect(() => {
-    console.log(`text is update ${text}`);
-  }, [text]);
+const Lifecycle = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggle = () => setIsVisible(!isVisible);
 
   return (
     <div style={{ padding: 20 }}>
-      <div>
-        {count}
-        <button onClick={() => setCount(count + 1)}>➕</button>
-      </div>
-      <div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-      </div>
+      <button onClick={toggle}>ON/OFF</button>
+      {/* 단락 회로 평가? 사용..(복습하기..) - isVisible이 True일 때 UnmountTest컴포넌트 화면에 렌더링 */}
+      {isVisible && <UnmountTest />}
     </div>
   );
 };
