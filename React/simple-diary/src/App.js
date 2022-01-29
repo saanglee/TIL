d5600633752652a1be4d21b17fea987a5e68a49f
Sangji <snagllee20@gmail.com>
2022-01-29 18:09:58 +0900

    API호출하기 - fetch

--- React/simple-diary/src/App.js
+++ React/simple-diary/src/App.js
@@ -1,13 +1,36 @@
 import "./App.css";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
+// import Lifecycle from "./Lifecycle";
 
 function App() {
   const [data, SetData] = useState([]);
 
-  const dataId = useRef(1);
+  const dataId = useRef(0);
+
+  // 함수 만들어서 API 호출하기
+  // fetch와 await를 함께 쓸것이기 때문에 async 키워드를 붙여준다. getData함수가 promise를 반환하는 비동기 함수되도록 함
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+    console.log(res);
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        create_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    SetData(initData);
+  };
+  // getData함수 호출 - App컴포넌트가 마운트 되자마자 (depth에 빈배열)
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -43,7 +66,7 @@ function App() {
 
   return (
     <div className="App">
-      <Lifecycle />
+      {/* <Lifecycle /> */}
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>
