b445d505f8d1322cc79e9b090d2564ebea3a9d90
Sangji <sangllee20@gmail.com>
2022-02-08 13:16:32 +0900

    7-2강 emotion diary 프로젝트 생성

---
+++ React/emotion-diary/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
