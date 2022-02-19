d6fefbcd70de2ad87fd9a70413e0d5c074274507
Sangji <sangllee20@gmail.com>
2022-02-19 18:05:41 +0900

    5.1강 세팅

---
+++ NomadCoder/react-for-beginners/src/App.js
@@ -0,0 +1,11 @@
+import Button from "./Button";
+function App() {
+  return (
+    <div className="App">
+      <h1>Wecome back!!</h1>
+      <Button text={"Continue"} />
+    </div>
+  );
+}
+
+export default App;
