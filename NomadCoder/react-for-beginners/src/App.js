95fbb80dcd2e8035baf0aeb14d569e114e44d923
Sangji <sangllee20@gmail.com>
2022-02-19 18:47:19 +0900

    6.2 useEffect

--- NomadCoder/react-for-beginners/src/App.js
+++ NomadCoder/react-for-beginners/src/App.js
@@ -1,10 +1,20 @@
-import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setVale] = useState(0);
+  const onClick = () => setVale((prev) => prev + 1);
+  console.log("i run all the time");
+
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
+
   return (
     <div className="App">
-      <h1 className={styles.title}>Wecome back!!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
