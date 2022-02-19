ab8271932011ba2bd3663da68dadbeb1f9b87e9f
Sangji <sangllee20@gmail.com>
2022-02-19 18:27:06 +0900

    5.1강 CSS module : css에서 클래스 중첩 방지가 가능하다.

--- NomadCoder/react-for-beginners/src/App.js
+++ NomadCoder/react-for-beginners/src/App.js
@@ -1,8 +1,9 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 function App() {
   return (
     <div className="App">
-      <h1>Wecome back!!</h1>
+      <h1 className={styles.title}>Wecome back!!</h1>
       <Button text={"Continue"} />
     </div>
   );
