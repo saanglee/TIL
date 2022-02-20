c61def6155e454c8719ef6908a4c587f5f1c3815
Sangji <sangllee20@gmail.com>
2022-02-20 17:29:39 +0900

    7-6강 (7) 날짜&일기내용, 수정버튼 구현, CSS 및 페이지 이동 기능 구현

--- React/emotion-diary/src/components/DiaryItem.js
+++ React/emotion-diary/src/components/DiaryItem.js
@@ -1,10 +1,28 @@
+import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
+
 const DiaryItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
+
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
+
+  // 일기 조회페이지 이동 함수
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+
+  // 일기 수정 이동 함수
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
 
   return (
     <div className="DiaryItem">
       <div
+        onClick={goDetail}
         className={[
           "emotion_img_wrapper",
           `emotion_img_wrapper_${emotion}`,
@@ -12,8 +30,14 @@ const DiaryItem = ({ id, emotion, content, date }) => {
       >
         <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
       </div>
-      <div></div>
-      <div></div>
+      <div onClick={goDetail} className="Info_wrapper">
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">{content.slice(0, 25)}</div>
+        {/* content가 너무 길면 내용이 잘릴 수 있으니까 slice */}
+      </div>
+      <div className="btn_wrapper">
+        <MyButton onClick={goEdit} text={"수정하기"} />
+      </div>
     </div>
   );
 };
