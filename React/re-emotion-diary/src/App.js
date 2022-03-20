bac0abff8f5d1fc3371ecc01ee675901cccdfa93
[3기 - 2반] 이상지 <sangji9504@gmail.com>
2022-03-20 19:01:52 +0900

    review: 3/20 7-5강 (1)정리

--- React/re-emotion-diary/src/App.js
+++ React/re-emotion-diary/src/App.js
@@ -14,34 +14,6 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <MyHeader
-          headText={"App"}
-          leftChild={
-            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
-          }
-          rightChild={
-            <MyButton
-              text={"오른쪽 버튼"}
-              onClick={() => alert("오른쪽 클릭")}
-            />
-          }
-        />
-        <h1>App.js</h1>
-
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"positive"}
-        />
-
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"negative"}
-        />
-
-        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
