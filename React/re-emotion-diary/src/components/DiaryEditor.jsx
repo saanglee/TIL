cb0710206ca578d1f3da189d76715a90545bd9f6
[3기 - 2반] 이상지 <sangji9504@gmail.com>
2022-04-18 15:30:18 +0900

    7-7: DiaryEditor, EmotionItem (오늘의 감정 섹션 구현)

--- React/re-emotion-diary/src/components/DiaryEditor.jsx
+++ React/re-emotion-diary/src/components/DiaryEditor.jsx
@@ -3,6 +3,38 @@ import React, { useState } from "react";
 
 import MyHeader from ".//MyHeader";
 import MyButton from "./MyButton";
+import EmotionItem from "./EmotionItem";
+
+// 감정 이미지 가져오기
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || "";
+const emotionList = [
+  {
+    emotion_id: 1,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
+    emotion_descript: "완전 좋음",
+  },
+  {
+    emotion_id: 2,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
+    emotion_descript: "좋음",
+  },
+  {
+    emotion_id: 3,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
+    emotion_descript: "그럭저럭",
+  },
+  {
+    emotion_id: 4,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
+    emotion_descript: "나쁨",
+  },
+  {
+    emotion_id: 5,
+    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
+    emotion_descript: "느어무 나쁨",
+  },
+];
 
 // date객체를 전달받아 년-월-일로 변환하는 함수
 const getStringDate = (date) => {
@@ -10,8 +42,13 @@ const getStringDate = (date) => {
 };
 
 const DiaryEditor = () => {
-  // console.log(getStringDate(new Date())); // getStringDate 함수 확인
+  const [emotion, setEmotion] = useState(3); // EmotionItem클릭 시 emotion state 값이 바뀌도록 한다.
   const [date, setDate] = useState(getStringDate(new Date())); // 오늘 날짜를 초기값으로 설정하기
+
+  const handleClickEmote = (emotion) => {
+    setEmotion(emotion);
+  };
+
   const navigate = useNavigate();
   return (
     <div className="DiaryEditor">
@@ -31,6 +68,20 @@ const DiaryEditor = () => {
               onChange={(e) => setDate(e.target.value)}
               type="date"
             />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              // <div key={it.emotion_id}>{it.emotion_descript}</div> 지우고, EmotionItem.jsx를 렌더링
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                isSelected={it.emotion_id === emotion}
+              />
+            ))}
           </div>
         </section>
       </div>
