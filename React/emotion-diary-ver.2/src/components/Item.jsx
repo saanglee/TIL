f8ca8827479ecc502a5becb7aad316fec591948d
sangji <sangllee20@gmail.com>
2022-05-06 18:01:47 +0900

    refactor: apply semantic markup and feat: make useNavigate to edit btn, article tag and make strDate func

--- React/emotion-diary-ver.2/src/components/Item.jsx
+++ React/emotion-diary-ver.2/src/components/Item.jsx
@@ -1,24 +1,36 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
 
 const Item = ({ id, title, author, rating, date, content }) => {
+  const navigate = useNavigate();
+  // TODO: toLocaleDateString 메서드 공부
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
+
+  const goDetail = () => {
+    navigate(`review/${id}`);
+  };
+  const goEdit = () => {
+    navigate(`edit/${id}`);
+  };
+
   return (
     <div className="DiaryItem">
-      <div className="info_wrapper">
+      <article className="info_wrapper" onClick={goDetail}>
         {/* title & author area */}
-        <div className="title_author">
-          {title}
-          {author}
-        </div>
+        <h2 className="h2_title">{title}</h2>
+        <h3 className="h3_author"> {author}</h3>
         {/* rating area */}
-        <div className="stars">별점</div>
+        <p className="stars">{rating}</p>
         {/* date & content area */}
-        <div className="date_content">작성일, 날짜</div>
-        {/* button area */}
-        <div className="btn_wrapper">
-          <MyButton text={"수정하기"} onClick={() => {}} />
-          <MyButton text={"삭제하기"} onClick={() => {}} />
-        </div>
+        <p className="p_date">{strDate}</p>
+        <p className="p_content">{content.slice(0, 25)}</p>
+      </article>
+      {/* button area */}
+      <div className="btn_wrapper">
+        <MyButton text={"수정하기"} onClick={goEdit} />
+        {/* TODO: 삭제하기 버튼 기능 구현 */}
+        <MyButton text={"삭제하기"} onClick={() => {}} />
       </div>
     </div>
   );
