fddbdc633b181f24db540363e5de4c329cc4cb16
sangji <sangllee20@gmail.com>
2022-05-06 17:46:31 +0900

    feat: Item markup and get props from List

--- React/emotion-diary-ver.2/src/components/Item.jsx
+++ React/emotion-diary-ver.2/src/components/Item.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import MyButton from "./MyButton";
 
-const Item = () => {
-  return <div></div>;
+const Item = ({ id, title, author, rating, date, content }) => {
+  return (
+    <div className="DiaryItem">
+      <div className="info_wrapper">
+        {/* title & author area */}
+        <div className="title_author">
+          {title}
+          {author}
+        </div>
+        {/* rating area */}
+        <div className="stars">별점</div>
+        {/* date & content area */}
+        <div className="date_content">작성일, 날짜</div>
+        {/* button area */}
+        <div className="btn_wrapper">
+          <MyButton text={"수정하기"} onClick={() => {}} />
+          <MyButton text={"삭제하기"} onClick={() => {}} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Item;
