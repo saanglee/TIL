69677598b2f5f025289ddbddb9ac3d4fb7eaa378
sangji <sangllee20@gmail.com>
2022-06-27 19:19:19 +0900

    Change directory

---
+++ javascript/methods/arr_methods.js
@@ -0,0 +1,8 @@
+// shift() 메서드는 배열에서 첫 번째 요소를 제거하고, 제거된 요소를 반환
+
+const array1 = [1, 2, 3];
+
+const firstElement = array1.shift();
+
+console.log(`array1: ${array1}`); // 2, 3
+console.log(`firstElement: ${firstElement}`); // 1
